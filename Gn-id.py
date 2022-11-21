02b5dd865c76556bfa1822d5575a463c3d795c24
Anas-Allhyani <118107911+Anas-Allhyani@users.noreply.github.com>
2022-11-21 15:55:23 +0300

    Gn-id.py

---
+++ Gn-id.py
@@ -0,0 +1,14 @@
+# coded by Anas Allhyani
+# my facebook account : https://www.facebook.com/Asdfrew90
+# Note: inside script i==1000:  its means how much idz you can edit this script
+# usage: python Gn-id.py > filename.txt
+import random
+x = 111111111
+xx = 999999999
+i=0
+while True:
+       em = random.randint(x,xx)
+       print('100000'+str(em))
+       i+=1
+       if i==1000:
+             break
